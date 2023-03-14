6e19a035a95ca5eeed3bed659fee661c1c3fcc5f
ohyoodan <45622048+ohyoodan@users.noreply.github.com>
2023-03-15 01:10:03 +0900

    ohyoodan 프로그램작성완료

--- cardinal_change/roop.hpp
+++ cardinal_change/roop.hpp
@@ -11,7 +11,7 @@ namespace cardinal_change {
 	class cardinal_change_Loop {
 		bool output = false;//값이 나올지 안나올지 판단
 		bool start = false;// 처음 실행 판단
-		std::vector<int> Test; //입력 구조자료
+		std::vector<int> numinput; //입력 구조자료
 		std::string str; //임시 입력
 		std::string outcardinal;// 출력 구조자료
 		bool oryu = false;
@@ -55,8 +55,11 @@ namespace cardinal_change {
 		  void Update() {
 			  output = false;
 			  if (start) {
-				  chack();
-				  
+				 // chack();
+				  change();
+				  for (const auto& num : numinput) {
+					  outcardinal = to_roman(num);
+				  }
 				  
 				  
 				  
@@ -70,9 +73,9 @@ namespace cardinal_change {
 		  void Render() {
 			  if (output) {//결과 출력
 				  std::cout << "로마숫자:" ;
-				  for (const auto& n : Test) {
-					  std::cout << n << " "<<std::endl;
-				  }
+				  
+					  std::cout << outcardinal << " "<<std::endl;
+				  
 				  std::cout << std::endl;
 			  }
 			  start = true;
@@ -89,8 +92,8 @@ namespace cardinal_change {
 		  void Release() {
 
 		  }
-		  void chack() {
-			  Test.clear();
+		  void chack() {//상수 문자 판별 미구현
+			  numinput.clear();
 			  if (str.empty() || (str.length() == 1 && !isdigit(str[0]))) {//문자열 비어 있을 경우 false
 				  //반환 false
 			}
@@ -101,19 +104,36 @@ namespace cardinal_change {
 			  }
 		  }
 
-		  void change() {
+		  void change() {//문자열>> 상수 변환기
 			  std::istringstream iss(str);//문자열을 분리해 의미있는 데이터로 변환하는 라이브러리
 			  int num;
 			  iss >> num;//변환 
-			  Test.push_back(num);//자료 구조에 넣기
+			  numinput.push_back(num);//자료 구조에 넣기
 			  output = true;
-			  switch (1)
-			  {
-			  default:
-				  break;
+			  
+			 
+		  }
+		  std::string to_roman(int num) {//상수>로마숫자 변환기
+			  std::string result;
+			  const int values[] = {//변하지 않는 배열
+				  1000, 900, 500, 400,
+				  100, 90, 50, 40,
+				  10, 9, 5, 4,
+				  1
+			  };
+			  const std::string symbols[] = {//변하지 않는 배열
+				  "M", "CM", "D", "CD",
+				  "C", "XC", "L", "XL",
+				  "X", "IX", "V", "IV",
+				  "I"
+			  };
+			  for (int i = 0; i < 13; ++i) {
+				  while (num >= values[i]) {
+					  num -= values[i];
+					  result += symbols[i];
+				  }
 			  }
-
-
+			  return result;
 		  }
 	};
 
